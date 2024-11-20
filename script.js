fa5a80381317004b9d7ebf48cd34e4a6213f74fa
Joseph Nahas <joe_nahas@hotmail.com>
2024-11-20 16:28:22 -0500

    Added Keydown Event

--- script.js
+++ script.js
@@ -8,3 +8,14 @@ hoverButton.addEventListener('mouseenter', function(){
 hoverButton.addEventListener('mouseleave', function(){
     hoverMessage.innerHTML = 'Come back!';
 });
+
+let keyInput = document.getElementById('keyboard');
+let keyGuess = document.getElementById('guess');
+
+keyInput.addEventListener('keydown', function(event){
+    
+    if (event.key != ' ')
+        keyGuess.innerHTML = event.key;
+    else
+        keyGuess.innerHTML = 'Spacebar';
+});
